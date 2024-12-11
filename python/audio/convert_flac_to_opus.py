c8b1830fb5363bfa9487aff65cb21f8af992b00c
mihmin98 <mihmin98@yahoo.com>
2024-12-11 15:29:27 +0200

    modified convert flac script

--- python/audio/convert_flac_to_opus.py
+++ python/audio/convert_flac_to_opus.py
@@ -1,18 +1,19 @@
 import subprocess
 from pathlib import Path
 import argparse
-import sys
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('bad number of args')
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('music_dir', type=str, help='Directory which contains the flac files')
+    parser.add_argument('--parent-dir-name', action='store_true', help='Use the parent dir name for the output dir, if not set, \"output_opus\" will be used')
 
-    if sys.argv[1].endswith('"'):
-        src_dir = Path(sys.argv[1][:-1])
+    args = parser.parse_args()
+
+    src_dir = Path(args.music_dir)
+    if args.parent_dir_name:
+        dest_dir = src_dir / src_dir.parts[-1]
     else:
-        src_dir = Path(sys.argv[1])
-
-    dest_dir = src_dir / 'output_opus'
+        dest_dir = src_dir / 'output_opus'
     dest_dir.mkdir(exist_ok=True)
 
     flac_files = list(src_dir.glob('*.flac'))
@@ -21,4 +22,7 @@ if __name__ == '__main__':
         output_path = dest_dir / flac_file.name.replace('.flac', '.opus')
         cmd = ['ffmpeg', '-i', str(flac_file), '-c:v', 'libtheora', '-q:v', '10', '-c:a', 'libopus', '-b:a', '160k', str(output_path)]
         print(' '.join(cmd))
-        subprocess.run(cmd, text=True)
+        subprocess.run(cmd, text=True)        
+
+if __name__ == '__main__':
+    main()
