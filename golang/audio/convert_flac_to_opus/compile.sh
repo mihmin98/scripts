3aa01f9011db188e8de56cee8b651751274dbba7
mihmin98 <mihmin98@yahoo.com>
2026-02-06 18:44:43 +0200

    Removed old golang scripts

--- golang/audio/convert_flac_to_opus/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p build/;
-GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o build/convert_flac_to_opus convert_flac_to_opus.go;
-GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o build/convert_flac_to_opus.exe convert_flac_to_opus.go;
